ab4563061c205f87af556e035d255ab28858da18
BenoitRichard1212 <33323670+BenoitRichard1212@users.noreply.github.com>
2019-09-26 14:02:53 -0400

    Create create.blade.php

---
+++ resources/views/global_settings/create.blade.php
@@ -0,0 +1,48 @@
+@extends('global_settings.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Global Settings</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('global_settings.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('global_settings.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <input type="text" name="name" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Value:</strong>
+                <textarea class="form-control" style="height:150px" name="value" placeholder="Value"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
