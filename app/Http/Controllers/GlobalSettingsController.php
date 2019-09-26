a06b0b56b8ee26ecc38ad4a337f7078ab17d88b5
BenoitRichard1212 <benoit.richard@novadba.com>
2019-09-26 15:01:19 -0230

    Changed controller for global settings

--- app/Http/Controllers/GlobalSettingsController.php
+++ app/Http/Controllers/GlobalSettingsController.php
@@ -2,24 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\global_settings;
 use Illuminate\Http\Request;
 
 class GlobalSettingsController extends Controller
 {
-    public function create()
-    {
-        return view('global_settings.create');
-    }
-    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $gsettings = global_settings::all();
-        return view('gsettings', ['gsettings' => $gsettings]);
+        //
     }
 
-    public function getSetting($p_setting)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        $setting = global_settings::where('gsettings', $p_gsetting->name)->first();
-        return $gsetting;
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\global_settings  $global_settings
+     * @return \Illuminate\Http\Response
+     */
+    public function show(global_settings $global_settings)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\global_settings  $global_settings
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(global_settings $global_settings)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\global_settings  $global_settings
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, global_settings $global_settings)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\global_settings  $global_settings
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(global_settings $global_settings)
+    {
+        //
     }
 }
