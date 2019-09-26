bb5a79832db8e2dcdc3410465770eb9b5bec5ac8
BenoitRichard1212 <33323670+BenoitRichard1212@users.noreply.github.com>
2019-09-26 18:33:35 -0400

    Update global_settings.php

--- app/global_settings.php
+++ app/global_settings.php
@@ -8,6 +8,7 @@ class global_settings extends Model
 {
     //Model pour la table : global_settings;
     protected $primaryKey = null;
+    public $incrementing = false;
     protected $table = 'global_settings';
     public $timestamps = false;
     protected $fillable = [
