5a8866133295ddea585a088d9d2472964a1b2443
BenoitRichard1212 <33323670+BenoitRichard1212@users.noreply.github.com>
2019-09-26 18:13:42 -0400

    Update edit.blade.php

--- resources/views/global_settings/edit.blade.php
+++ resources/views/global_settings/edit.blade.php
@@ -23,7 +23,7 @@
         </div>
     @endif
   
-    <form action="{{ route('global_settings.update',$product->id) }}" method="POST">
+    <form action="{{ route('global_settings.update',$global_settings->name) }}" method="POST">
         @csrf
         @method('PUT')
    
@@ -31,13 +31,13 @@
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Name:</strong>
-                    <input type="text" name="name" value="{{ $global_setting->name }}" class="form-control" placeholder="Name">
+                    <input type="text" name="name" value="{{ $global_settings->name }}" class="form-control" placeholder="Name">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Detail:</strong>
-                    <textarea class="form-control" style="height:150px" name="value" placeholder="Value">{{ $global_setting->value }}</textarea>
+                    <strong>Value:</strong>
+                    <textarea class="form-control" style="height:150px" name="value" placeholder="Value">{{ $global_settings->value }}</textarea>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
