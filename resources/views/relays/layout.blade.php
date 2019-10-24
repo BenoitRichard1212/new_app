1c3af89be1ff19d7bb00cbbbc09f226e7b1b8c5c
BenoitRichard1212 <33323670+BenoitRichard1212@users.noreply.github.com>
2019-10-24 18:03:57 -0400

    Update layout.blade.php
    Added Menu Layout

--- resources/views/relays/layout.blade.php
+++ resources/views/relays/layout.blade.php
@@ -3,17 +3,89 @@
 <head>
     <title>Relays</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Heating App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .full-height {
+                height: 50vh;
+            }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+            .content {
+                text-align: center;
+            }
+            .title {
+                font-size: 17px;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .m-b-md {
+                margin-bottom: 10px;
+            }
+        </style>
 </head>
 <body>
 
-<b> HEATING </b>
+<div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
 
-<p>
-   <a href="{{ url('rooms') }}">Rooms</a>
-   <a href="{{ url('sensors') }}">Sensors</a>
-   <a href="{{ url('relays') }}">Relays</a>
-   <a href="{{ url('global_settings') }}">Global Settings</a>
-</p>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Heating App
+                </div>
+
+                <div class="links">
+                    <a href="{{ url('rooms') }}">Rooms</a>
+                    <a href="{{ url('sensors') }}">Sensors</a>
+                    <a href="{{ url('relays') }}">Relays</a>
+                    <a href="{{ url('global_settings') }}">Global Settings</a>
+                </div>
+            </div>   
+</div>
 
 <div class="container">
     @yield('content')
