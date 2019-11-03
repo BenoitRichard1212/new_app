1c2454a657d6d149336ce7b96bf858382d961431
Benoit Richard <benoit.richard@novadba.com>
2019-11-03 12:53:21 -0500

    try5

--- resources/views/global_settings/edit.blade.php
+++ resources/views/global_settings/edit.blade.php
@@ -28,12 +28,12 @@
         @method('POST')
    
          <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
+            <!--<div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Name:</strong>
                     <input type="text" name="name" value="{{ $global_settings->name }}" class="form-control" placeholder="{{ $global_settings->name }}">
                 </div>
-            </div>
+            </div>-->
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Value:</strong>
