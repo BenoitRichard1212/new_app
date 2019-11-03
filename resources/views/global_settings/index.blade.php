a251458522e23aa2fdb23487bc0a190cb1f5bdef
Benoit Richard <benoit.richard@novadba.com>
2019-11-03 13:30:14 -0500

    Big 2

--- resources/views/global_settings/index.blade.php
+++ resources/views/global_settings/index.blade.php
@@ -31,7 +31,7 @@
             <td>{{ $global_setting->name }}</td>
             <td>{{ $global_setting->value }}</td>
             <td>
-                <form action="{{ route('global_settings.destroy',$global_setting->name, $global_setting) }}" method="POST">
+                <td><a href="{{action('GlobalSettingsController@edit',$global_setting->name)}}" class="btn btn-primary">Edit</a></td>
    
                     <a class="btn btn-info" href="{{ route('global_settings.show',$global_setting->name) }}">Show</a>
     
