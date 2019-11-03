1ec856df8afe612c68a396e5cfe5f6ddd091d39a
Benoit Richard <benoit.richard@novadba.com>
2019-11-03 10:33:57 -0500

    5

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -30,7 +30,7 @@
         </tr>
         @foreach ($rooms as $room)
         <tr>
-            <td>{{ $room->temp_min }}</td>
+            <td>{{ $room->name }}</td>
             <td>{{ ++$i }}</td>
             <td>{{ $room->name }}</td>
             <td>{{ $room->temp_min }}</td>
